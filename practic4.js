a9c77455d06aed0615de3f469d938a58de27d925
kategrom <95121842+kategrom@users.noreply.github.com>
2022-07-07 15:46:44 +0300

    Add files via upload

---
+++ practic4.js
@@ -0,0 +1,75 @@
+"use strict";
+
+// Task 1
+
+// const num = +prompt("Введіть число");
+// if (num >= 55 && num <= 99){
+//   console.log("Число потрапляє у діапазон")
+// }else{
+//   console.log("Число не потрапляє у діапазон")
+// }
+
+
+
+// Task 2
+
+// const num = +prompt("Введіть свій вік");
+// if (num >= 0 && num <= 16){
+//   console.log("Ви дитина");
+// } else if (num >= 17 && num <= 60){
+//   console.log("Ви дopocлі");
+// } else if (num >= 61 && num <= 100){
+//   console.log("Ви пенсіонер");
+// }
+
+// Task 3
+
+// const name = prompt("Введіть своє ім'я");
+//     const surname = prompt('Введіть своє прізвище');
+//     const all = name + ' ' + surname;
+//     if (name.length > 4 && surname.length > 5) {
+//         console.log(all);
+//         console.log(name + ' - ' + name.length);
+//         console.log(surname + ' - ' + surname.length);
+//         console.log(name.length + ' + ' + surname.length + ' = ' + (name.length + surname.length));
+//         console.log(all + ' - ' + (name.length + surname.length));
+//     } else {
+//         console.log("Упс, ваше ім'я або прізвище занадто коротке");
+//     }
+
+
+// Task 4
+
+// const ranNum = Math.round(Math.random()* (5 - 1)) + 1;
+// if(num === 1){
+//   console.log("Це 1")
+// } else if (num === 2){
+//   console.log("Це 2")
+// } else if (num === 3){
+//   console.log("Це 3")
+// } else if (num === 4){
+//   console.log("Це 4")
+// } else if (num === 5){
+//   console.log("Це 5")
+// }
+
+
+// Task 5
+
+// const lang = prompt("Введіть мову");
+// switch (lang) {
+//   case "ua":
+//     console.log("Січень");
+//     break;
+//   case "en":
+//       console.log("January");
+//     break;
+//   case "fr":
+//     console.log("Janvier");
+//     break;
+//   case "ru":
+//     console.log("Январь");
+//     break;  
+//   default:
+//     alert("Цієї мови немає у програмі")  
+// }
